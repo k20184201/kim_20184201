e57bc5bfddc766c7e431441acd9df6ee36e639da
k20184201 <89475243+k20184201@users.noreply.github.com>
2021-09-05 16:19:55 +0900

    Add files via upload

---
+++ 2/2.4.1-3.js
@@ -0,0 +1,12 @@
+inport{Fragment}from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+
+    </Fragment>
+  );
+}
+export default App;
---
+++ 2/2.4.1-4.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </>
+  );
+}
+export default App;
