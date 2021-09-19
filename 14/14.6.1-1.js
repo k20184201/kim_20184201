2c6c7270666f241ba6990cb836819dbd3a33686e
k20184201 <89475243+k20184201@users.noreply.github.com>
2021-09-19 16:50:36 +0900

    Add files via upload

---
+++ 14/14.6.1-1.js
@@ -0,0 +1,13 @@
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  return (
+    <>
+      <Categories />
+      <NewsList />
+    </>
+  );
+};
+
+export default App;
---
+++ 14/14.6.1-2.js
@@ -0,0 +1,17 @@
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+import { useState, useCallback } from 'react';
+
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default App;
