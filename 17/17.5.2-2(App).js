9b4b220d1bb1cc70f547cf7a9cc20dc5dc2605c2
k20184201 <89475243+k20184201@users.noreply.github.com>
2021-09-27 01:50:18 +0900

    Add files via upload

---
+++ 17/17.5.2-2(App).js
@@ -0,0 +1,14 @@
+import CounterContainer from './containers/17.5.1-7(CounterContainer)';
+import TodosContainer from './containers/17.5.2-1(TodosContainer)';
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
+    </div>
+  );
+};
+
+export default App;
